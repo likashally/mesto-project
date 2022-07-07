0e7e37302ce44dec25fdc5f7e1536bd7c6d43ca8
likashally <likashally@gmail.com>
2022-07-08 01:57:03 +0800

    added name for forms and started form validation

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,9 @@ const imageFull = document.querySelector('.popup_type_image');
 const popupImage = document.querySelector('.popup__image');
 const popupTitle = document.querySelector('.popup__image-title');
 
+//popup save button
+const submitButton = document.querySelector('.popup__save-btn');
+
 
 function openPopup(popup) {
     popup.classList.add('popup_opened');
@@ -25,15 +28,15 @@ function closePopup(popup) {
 
 popupCloseButton.forEach(closeButton => closeButton.addEventListener("click", closeButtonAction));
 
-function closeButtonAction(evt){
+function closeButtonAction(evt) {
     closePopup(evt.target.closest(".popup"));
 }
 
-function editButtonAction(){
+function editButtonAction() {
     nameInput.value = profileName.textContent;
     jobInput.value = profileOccupation.textContent;
     openPopup(profilePopup);
-    
+
 }
 
 function addButtonAction() {
@@ -44,56 +47,69 @@ profileEditButton.addEventListener('click', editButtonAction);
 cardAddButton.addEventListener('click', addButtonAction);
 
 // Находим форму в DOM
-
-const formElement = document.querySelector('#profile-form');
-// Воспользуйтесь методом querySelector()
+const formElement = document.forms.profileForm;
 
 // Находим поля формы в DOM
 
-const nameInput = document.querySelector('#input__name');
-// Воспользуйтесь инструментом .querySelector()
-
-const jobInput = document.querySelector('#input__occupation');
-// Воспользуйтесь инструментом .querySelector()
-
+const nameInput = document.profileForm.elements.name;
+const jobInput = document.profileForm.elements.occupation;
 
 // Обработчик «отправки» формы, хотя пока
 
 // она никуда отправляться не будет
 
-    function formSubmitHandler (evt) {
-        evt.preventDefault();
-        profileName.textContent = nameInput.value;
-        profileOccupation.textContent = jobInput.value;
-        closePopup(profilePopup);
-    }
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileOccupation.textContent = jobInput.value;
+    closePopup(profilePopup);
+}
+
+function setSubmitButtonState(isFormValid){
+    if (isFormValid) {
+        submitButton.removeAttribute('disabled');
+        submitButton.classList.remove('popup__save-btn_disabled');
+      } else {
+        submitButton.setAttribute('disabled', true);
+        submitButton.classList.add('popup__save-btn_disabled');
+      }
+}
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
+formElement.addEventListener('input', function (evt) {
+    if (artist.value.length > 0 && title.value.length > 0) {
+      isValid = true;
+    }
+    else {
+      isValid = false;
+    }
+});
 
-function createCardElement(name, link){
+
+function createCardElement(name, link) {
     const element = elementTemplate.querySelector(".element").cloneNode(true);
     const imageElement = element.querySelector(".element__img");
     imageElement.src = link;
     imageElement.alt = name;
-    imageElement.addEventListener("click", function(){imageClickHandler(name, link)});
+    imageElement.addEventListener("click", function () { imageClickHandler(name, link) });
     element.querySelector(".element__title").textContent = name;
     element.querySelector(".element__heart-btn").addEventListener("click", likeToggle);
     element.querySelector(".element__trash").addEventListener("click", removeCard);
     return element;
 }
 
-function prependCard(list, name, link){
+function prependCard(list, name, link) {
     list.prepend(createCardElement(name, link));
 }
 
-function submitForm(evt){
+function submitForm(evt) {
     evt.preventDefault();
     prependCard(cardsContainer, title.value, image.value);
     closePopup(cardPopup);
-    title.value = ""; 
+    title.value = "";
     image.value = "";
 }
 
@@ -107,11 +123,11 @@ function likeToggle(evt) {
     evt.target.classList.toggle("element__heart-btn_active");
 }
 
-function removeCard(evt){
+function removeCard(evt) {
     evt.target.closest(".element").remove();
 }
 
-function imageClickHandler(name, link){
+function imageClickHandler(name, link) {
     popupImage.src = link;
     popupImage.alt = name;
     popupTitle.textContent = name;
